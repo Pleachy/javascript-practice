e77dd50fb7b4cc806180b8e1815614d0334412ce
Pleachy <daniel.golub@gmail.com>
2022-01-24 21:14:33 -0800

    initialized player context and player class

--- shooter.js
+++ shooter.js
@@ -1,7 +1,22 @@
 //grab the canvas element from our html file
 const canvas = document.querySelector('canvas');
 
-//set canvas width to be the size of the screen
-canvas.width = innerWidth;
+//getting player context, specifically for a 2d environment
+const context = canvas.getContext('2d');
 
-console.log(canvas)
+//set canvas width to be the size of the screen, innerwWidth is a property of the window object
+canvas.width = innerWidth;
+//set canvas height to be the size of the screen, innerHeight is a property of the window object
+canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+console.log(context);
+
