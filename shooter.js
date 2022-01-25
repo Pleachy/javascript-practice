91cc3381a2c479fd522630c9b4ec4d3dc6b9cd49
Pleachy <daniel.golub@gmail.com>
2022-01-24 21:08:40 -0800

    file initialization

---
+++ shooter.js
@@ -0,0 +1,7 @@
+//grab the canvas element from our html file
+const canvas = document.querySelector('canvas');
+
+//set canvas width to be the size of the screen
+canvas.width = innerWidth;
+
+console.log(canvas)
